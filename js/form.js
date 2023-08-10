b5425c9d1711dd25e749d21578339ff05bc6fbb7
crazyro15 <crazyro15@hotmail.com>
2023-08-10 12:28:37 -0300

    validacion e iconos ok

--- js/form.js
+++ js/form.js
@@ -1,18 +1,18 @@
-window.onload = function() {
+// window.onload = function() {
 	
-    var nombre = document.getElementById('txtNombre');
-    nombre.addEventListener("input", campoNombre);
+//     var nombre = document.getElementById('txtNombre');
+//     nombre.addEventListener("input", campoNombre);
 	
-    var correo = document.getElementById('txtPrecio');
-    correo.addEventListener("input", campoCorreo);
+//     var correo = document.getElementById('txtPrecio');
+//     correo.addEventListener("input", campoCorreo);
 
-    var mensaje = document.getElementById('txtStock');
-    mensaje.addEventListener("input", campoMensaje);
+//     var mensaje = document.getElementById('txtStock');
+//     mensaje.addEventListener("input", campoMensaje);
 	
-}
+// }
 
-function campoNombre() {
-    var usuarioNombre = document.getElementById('txtNombre').value;
+function campoNombref() {
+    let usuarioNombre = document.getElementById('txtNombre').value;
 
     if (usuarioNombre) {
         document.getElementById('nombre_error').innerHTML = " ";
@@ -20,8 +20,8 @@ function campoNombre() {
     }
 }
 
-function campoCorreo() {
-    var usuarioCorreo = document.getElementById('txtPrecio').value;
+function campoCorreof() {
+    let usuarioCorreo = document.getElementById('txtPrecio').value;
 	
     if (usuarioCorreo) {
         document.getElementById('correo_error').innerHTML = " ";
@@ -29,8 +29,8 @@ function campoCorreo() {
     }
 }
 
-function campoMensaje() {
-    var usuarioMensaje = document.getElementById('txtStock').value;
+function campoMensajef() {
+    let usuarioMensaje = document.getElementById('txtStock').value;
 	
     if (usuarioMensaje) {
         document.getElementById('mensaje_error').innerHTML = " ";
@@ -42,45 +42,51 @@ function borrando() {
 	document.location.reload(true);
 }
 
+document.getElementById("formContacto").addEventListener("submit", function(event) {
+    if (!validacion()) {
+      event.preventDefault(); // Previene el envío del formulario si la validación falla
+    }
+  });
+
 function validacion() {
-    var expUsuario = /[a-zA-Z]{2,30}[0-9]{0}/;
-    var expCorreo = /^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/;
-    var usuarioNombre = document.getElementById('txtNombre').value;
-    var usuarioCorreo = document.getElementById('txtPrecio').value;
-    var usuarioMensaje = document.getElementById('txtStock').value;
-    var nombreError = document.getElementById('nombre_error');
-    var correoError = document.getElementById('correo_error');
-    var mensajeError = document.getElementById('mensaje_error');
-    var campoNombre = document.getElementById('txtNombre');
-    var campoCorreo = document.getElementById('txtPrecio');
-    var campoMensaje = document.getElementById('txtStock');
+    let expUsuario = /[a-zA-Z]{2,30}[0-9]{0}/;
+    let expCorreo = /^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/;
+    let usuarioNombre = document.getElementById('txtNombre').value;
+    let usuarioCorreo = document.getElementById('txtPrecio').value;
+    let usuarioMensaje = document.getElementById('txtStock').value;
+    let nombreError = document.getElementById('nombre_error');
+    let correoError = document.getElementById('correo_error');
+    let mensajeError = document.getElementById('mensaje_error');
+    let campoNombre = document.getElementById('txtNombre');
+    let campoCorreo = document.getElementById('txtPrecio');
+    let campoMensaje = document.getElementById('txtStock');
 
     if (!usuarioNombre) {
-    	var mensajeErrorNombre = "Su nombre es obligatorio!";
+    	let mensajeErrorNombre = "Su nombre es obligatorio!";
         nombreError.innerHTML = mensajeErrorNombre;
         campoNombre.style.outline = "2px solid #0f0";
         return false;
     } else if (!expUsuario.test(usuarioNombre)) {
-        var mensajeInvalidoNombre = "Su nombre completo es invalido!";
+        let mensajeInvalidoNombre = "Su nombre completo es invalido!";
         nombreError.innerHTML = mensajeInvalidoNombre;
         campoNombre.style.outline = "2px solid #f00";
         return false;
     }
 	
     if (!usuarioCorreo) {
-    	var mensajeErrorCorreo = "Su correo electrónico es obligatorio!";
+    	let mensajeErrorCorreo = "Su correo electrónico es obligatorio!";
     	correoError.innerHTML = mensajeErrorCorreo;
     	campoCorreo.style.outline = "2px solid #0f0";
     	return false;
     } else if (!expCorreo.test(usuarioCorreo)) {
-    	var mensajeInvalidoCorreo = "correo inválido!(Sin mayúsculas ni espacios)";
+    	let mensajeInvalidoCorreo = "correo inválido!(Sin mayúsculas ni espacios)";
     	correoError.innerHTML = mensajeInvalidoCorreo;
     	campoCorreo.style.outline = "2px solid #f00";
     	return false;
     }
 	
     if (!usuarioMensaje) {
-    	var mensajeErrorMensaje = "Por favor, escriba su mensaje!";
+    	let mensajeErrorMensaje = "Por favor, escriba su mensaje!";
     	mensajeError.innerHTML = mensajeErrorMensaje;
     	campoMensaje.style.outline = "2px solid #0f0";
     	return false;
@@ -90,11 +96,10 @@ function validacion() {
     	campoMensaje.style.outline = "2px solid #f00";
     	return false;
     } 
-    campoNombre();
-    campoCorreo();
-    campoMensaje();
+    campoNombref();
+    campoCorreof();
+    campoMensajef();
     guardar();
-    console.log("Enviado")
 }
 
 function guardar() {
@@ -110,13 +115,24 @@ function guardar() {
     }
     //let url = "http://localhost:5000/mensajes"
     let url = "https://turismoclaromeco.onrender.com/mensajes"
-    var options = {
+    let options = {
         body: JSON.stringify(producto),
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
        // redirect: 'follow'
     }
-    
+    fetch(url, options)
+        .then(function () {
+            // console.log("creado")
+            location.href='enviado.html';
+            // Handle response we get from the API
+        })
+        .catch(err => {
+            //this.errored = true
+            alert("Ocurrió un error al guardar" )
+            console.error(err);
+        })
+        // windows.location.replace('back.html');
 }
 
 // function login() {
